9769437215a89a6a4e0aaa62b2729e1cfba84b0a
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-28 17:56:17 -0300

    Retira background dos botões no header

--- components/Layout/Head.vue
+++ components/Layout/Head.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg-gray-500 flex justify-end items-center gap-3 px-2">
-    <Button @click="toggleSidebar()">
+    <Button @click="toggleSidebar()" class="bg-transparent">
       <Icon :name="eyeIcon" size="20"></Icon>
     </Button>
-    <Button @click="toggleChat()">
+    <Button @click="toggleChat()" class="bg-transparent">
       <Icon name="bi:chat-fill" size="20"></Icon>
     </Button>
     <ColorModeToogle></ColorModeToogle>
