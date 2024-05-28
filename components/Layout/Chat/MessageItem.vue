5e9c1bf20a93916631f98f8951d5bc24fe29fb17
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-28 17:18:55 -0300

    Adiciona componente de mensagem

---
+++ components/Layout/Chat/MessageItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <!--<div
+    :class="getMessageClass(message.messageOwner)"
+    v-for="message in userMessages"
+    :key="message.content"
+  >
+    {{ message.content }}
+  </div>-->
+  <div>
+    <div v-for="item in 100" :key="item">oiilll</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useChatStore } from "@/stores/chat";
+const chatStore = useChatStore();
+
+const userMessages = computed(() => chatStore.messages);
+function getMessageClass(messageOwner: boolean) {
+  if (messageOwner) {
+    return "bg-red-500";
+  }
+  return "bg-blue-500";
+}
+</script>
+
+<style></style>
