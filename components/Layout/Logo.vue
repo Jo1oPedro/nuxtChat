1862dfb641d31f5105820994bf7fbdf5db002cae
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-28 18:49:03 -0300

    Adiciona component de logo

---
+++ components/Layout/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="overflow-hidden rounded-lg w-[45px]">
+    <NuxtImg src="/img/logo.png" alt="logo" />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
