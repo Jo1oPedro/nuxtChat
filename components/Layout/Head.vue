979fe7e80d5077abd85cea0ecc78cf5b3e04e51d
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-28 18:25:47 -0300

    Adiciona troca de cor dos icones baseado no cor do sistema escolhida pelo usuario

--- components/Layout/Head.vue
+++ components/Layout/Head.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-gray-500 flex justify-end items-center gap-3 px-2">
+  <div class="flex justify-end items-center gap-3 px-2">
     <Button @click="toggleSidebar()" class="bg-transparent">
-      <Icon :name="eyeIcon" size="20"></Icon>
+      <Icon :name="eyeIcon" size="20" :color="iconColor"></Icon>
     </Button>
     <Button @click="toggleChat()" class="bg-transparent">
-      <Icon name="bi:chat-fill" size="20"></Icon>
+      <Icon name="bi:chat-fill" size="20" :color="iconColor"></Icon>
     </Button>
     <ColorModeToogle></ColorModeToogle>
   </div>
@@ -13,6 +13,13 @@
 <script lang="ts" setup>
 defineProps(["eyeIcon"]);
 const emit = defineEmits(["toggleSidebar", "toggleChat"]);
+
+const colorMode = useColorMode();
+
+const iconColor = ref(colorMode.value === "dark" ? "white" : "black");
+watch(colorMode, function () {
+  iconColor.value = colorMode.value === "dark" ? "white" : "black";
+});
 
 function toggleSidebar() {
   emit("toggleSidebar");
