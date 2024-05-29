2e44b2bb602fe23d5c85196ee9cc2a2e6b5137f8
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-29 11:15:41 -0300

    Corrige tamanho do chato, passa a adiciona as mensagens no final do elemento e a utilizar o componenent te messageItem

--- components/Layout/Chat.vue
+++ components/Layout/Chat.vue
@@ -7,23 +7,34 @@
       @click="toggleChat"
     ></Icon>
     <div
-      class="absolute bottom-0 right-0 bg-red-500 w-[200px] h-[400px] md:w-[400px] md:h-[600px] flex flex-col"
+      class="absolute bottom-0 right-1 md:right-2 bg-neutral-100 w-[150px] h-[350px] md:w-[300px] md:h-[500px] flex flex-col"
       v-if="chatOpen"
     >
-      <div class="flex justify-between px-1 items-center">
+      <div class="flex justify-between p-1 items-center border-b-2">
         <UsersChatProfile name="Vitoria" image="img/vitoria.jpg" />
         <Icon
           name="material-symbols:close"
           size="30"
           @click="toggleChat"
+          color="black"
         ></Icon>
       </div>
-      <div class="overflow-y-scroll">
-        <div class="w-full grow bg-orange-200">
-          <LayoutChatMessageItem></LayoutChatMessageItem>
+      <div class="overflow-y-scroll h-full flex">
+        <div class="w-full grow flex flex-col justify-end">
+          <div
+            :class="getMessageClass(message.messageOwner)"
+            v-for="message in userMessages"
+            :key="message.content"
+          >
+            <!--{{ message.content }}-->
+            <LayoutChatMessageItem
+              :content="message.content"
+              :messageOwner="message.messageOwner"
+            ></LayoutChatMessageItem>
+          </div>
         </div>
       </div>
-      <div class="bg-red-500 p-1">
+      <div class="p-1">
         <Input
           class="w-full"
           type="text"
@@ -38,6 +49,7 @@
 
 <script lang="ts" setup>
 import { Input } from "@/components/ui/input";
+import { useChatStore } from "@/stores/chat";
 
 defineProps({
   chatOpen: {
@@ -49,6 +61,16 @@ defineProps({
 const emit = defineEmits(["toggleChat"]);
 function toggleChat() {
   emit("toggleChat");
+}
+
+const chatStore = useChatStore();
+
+const userMessages = computed(() => chatStore.messages);
+function getMessageClass(messageOwner: boolean) {
+  if (messageOwner) {
+    return "bg-red-500";
+  }
+  return "bg-blue-500";
 }
 
 const userMessage = ref("");
