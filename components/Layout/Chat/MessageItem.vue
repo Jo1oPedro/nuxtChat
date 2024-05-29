7dd23e0c6712457372318d353def3268e13024b7
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-29 11:16:51 -0300

    Adiciona props de conteudo da mensagem e dono

--- components/Layout/Chat/MessageItem.vue
+++ components/Layout/Chat/MessageItem.vue
@@ -1,21 +1,22 @@
 <template>
-  <!--<div
-    :class="getMessageClass(message.messageOwner)"
-    v-for="message in userMessages"
-    :key="message.content"
-  >
-    {{ message.content }}
-  </div>-->
   <div>
-    <div v-for="item in 100" :key="item">oiilll</div>
+    <div :class="getMessageClass(messageOwner)" class="text-base">
+      {{ content }}
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { useChatStore } from "@/stores/chat";
-const chatStore = useChatStore();
-
-const userMessages = computed(() => chatStore.messages);
+defineProps({
+  content: {
+    type: String,
+    required: true,
+  },
+  messageOwner: {
+    type: Boolean,
+    required: true,
+  },
+});
 function getMessageClass(messageOwner: boolean) {
   if (messageOwner) {
     return "bg-red-500";
