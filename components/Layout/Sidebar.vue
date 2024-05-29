3e61353ac91ce206e92ebf378c277dc0a5428892
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-29 16:48:21 -0300

    Adiciona sobreposição da sidebar em dispositivos sm

--- components/Layout/Sidebar.vue
+++ components/Layout/Sidebar.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="w-[220px] h-full border-r-2 flex flex-col gap-2">
+  <div
+    class="m-0 p-0 fixed md:relative z-50 md:z-0 w-full md:w-[220px] bg-white h-full flex flex-col gap-2 md:border-r-2"
+  >
     <header
-      class="flex items-center gap-2 hover:scale-[101%] transition cursor-pointer p-3"
+      class="flex items-center gap-2 hover:scale-[101%] transition cursor-pointer"
     >
-      <LayoutLogo />
-      <p class="font-bold text-sm md:text-base">Missing Pets</p>
+      <div class="flex items-center gap-2">
+        <LayoutLogo />
+        <p class="font-bold text-sm md:text-base">Missing Pets</p>
+      </div>
+      <Icon name="mdi:hide" size="20" @click="toggleSidebar()"></Icon>
     </header>
-    <div class="flex gap-2 flex-col px-2">
+    <div class="flex gap-2 flex-col p-2">
       <NuxtLink
         v-for="item in items"
         :key="item.title"
         :to="item.path"
-        class="flex items-center gap-2 px-2 py-1 rounded transition cursor-pointer hover:bg-neutral-100"
+        class="flex items-center gap-2 px-2 py-1 rounded transition cursor-pointer hover:bg-neutral-100 border-b-2"
       >
         <Icon :name="item.icon" />
         <span>{{ item.title }}</span>
@@ -21,6 +26,12 @@
 </template>
 
 <script lang="ts" setup>
+const emit = defineEmits(["toggleSidebar"]);
+
+function toggleSidebar() {
+  emit("toggleSidebar");
+}
+
 const items = ref([
   {
     title: "Home",
