48e4f8623a304520e5585481f2ec1243ad8f0dd6
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-05-30 18:23:53 -0300

    Adiciona validações de token ao middleware global de autenticação

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,17 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.fullPath !== "/login" && false) {
+  const { authenticated } = storeToRefs(useAuthStore());
+  const token = useCookie("token");
+
+  if (token.value) {
+    authenticated.value = true;
+  }
+
+  if (token.value && to?.name === "login") {
+    return navigateTo("/");
+  }
+
+  if (!token.value && to?.name !== "login") {
+    abortNavigation();
     return navigateTo("/login");
   }
 });
