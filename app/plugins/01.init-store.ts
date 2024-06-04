638723e18974bf4556c8f96da7da6a0ea2b5a9a9
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-04 18:26:56 -0300

    Adiciona components de dialogo e component para criação de posts de pets desaparecidos

---
+++ app/plugins/01.init-store.ts
@@ -0,0 +1,12 @@
+import { useAuthStore } from "~/stores/auth";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  /*if (process.server) return;
+
+  const authStore = useAuthStore();
+  const user = useCookie("authenticatedUser");
+
+  if (user.value) {
+    authStore.setUser(user);
+  }*/
+});
