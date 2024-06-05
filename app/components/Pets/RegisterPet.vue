b08071e480f16b0d025907a23e1a363da255f77b
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-05 10:45:52 -0300

    Altear comportamento do close dialog do shadcn e adiciona componente para cadastro de pets desaparecidos

--- app/components/Pets/RegisterPet.vue
+++ app/components/Pets/RegisterPet.vue
@@ -8,20 +8,129 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+import { vAutoAnimate } from "@formkit/auto-animate/vue";
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const open = ref(true);
+function toggleDialog() {
+  if (
+    open.value &&
+    confirm(
+      "Tem certeza que deseja sair? Todos os dados não salvos serão perdidos"
+    )
+  ) {
+    open.value = false;
+    return;
+  } else if (!open.value) {
+    open.value = true;
+  }
+}
+
+function onSubmit() {
+  alert("dALE");
+}
 </script>
 
 <template>
-  <Dialog>
-    <DialogTrigger> Edit Profile </DialogTrigger>
-    <DialogContent>
+  <Dialog :open="open">
+    <DialogTrigger>
+      <Button @click="toggleDialog">Cadastrar pet desaparecido</Button>
+    </DialogTrigger>
+    <DialogContent
+      @close="toggleDialog()"
+      @interact-outside="
+        (event) => {
+          //const target = event.target as HTMLElement;
+          //if (target?.closest('[data-sonner-toaster]'))
+          return event.preventDefault();
+        }
+      "
+    >
       <DialogHeader>
-        <DialogTitle>Edit profile</DialogTitle>
+        <DialogTitle></DialogTitle>
         <DialogDescription>
-          Make changes to your profile here. Click save when you're done.
+          <form class="flex flex-col gap-4 w-full" @submit.prevent="onSubmit">
+            <FormField v-slot="{ componentField }" name="name">
+              <FormItem v-auto-animate>
+                <FormLabel>Nome do pet</FormLabel>
+                <FormControl>
+                  <Input type="text" v-bind="componentField" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="coordinate_x">
+              <FormItem v-auto-animate>
+                <FormLabel>Coordenada x</FormLabel>
+                <FormControl>
+                  <Input type="number" v-bind="componentField" value="30" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="coordinate_y">
+              <FormItem v-auto-animate>
+                <FormLabel>Coordenada y</FormLabel>
+                <FormControl>
+                  <Input type="number" v-bind="componentField" value="30" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="breed">
+              <FormItem v-auto-animate>
+                <FormLabel>Raça</FormLabel>
+                <FormControl>
+                  <Input type="text" v-bind="componentField" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="type">
+              <FormItem v-auto-animate>
+                <FormLabel>Tipo</FormLabel>
+                <FormControl>
+                  <Input type="text" v-bind="componentField" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <FormField v-slot="{ componentField }" name="additional_info">
+              <FormItem v-auto-animate>
+                <FormLabel>Informações adicionais</FormLabel>
+                <FormControl>
+                  <Textarea class="resize-none" v-bind="componentField" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+            <div
+              v-if="error"
+              class="bg-red-500 rounded-full text-center text-base p-2 font-bold"
+            >
+              {{ error }}
+            </div>
+            <div class="flex justify-center">
+              <Button type="submit"> Cadastrar </Button>
+            </div>
+          </form>
         </DialogDescription>
       </DialogHeader>
 
-      <DialogFooter> Save changes </DialogFooter>
+      <DialogFooter></DialogFooter>
     </DialogContent>
   </Dialog>
 </template>
