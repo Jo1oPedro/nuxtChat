e260ad8fe1c1fa7394f15cd0d68ebc42b4327e3b
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-07 10:56:05 -0300

    Adiciona novas regras de validação

--- app/plugins/validationMessages.ts
+++ app/plugins/validationMessages.ts
@@ -3,6 +3,8 @@ const generateMessages = (fieldName: string) => ({
     `${fieldName} precisa ter pelo menos ${value} caracteres`,
   max: (value: string | number) =>
     `${fieldName} não pode exceder ${value} caracteres`,
+  minNumber: (value: string | number) => `${fieldName} precisa ser >= ${value}`,
+  maxNumber: (value: string | number) => `${fieldName} precisa ser <= ${value}`,
   required: `${fieldName} é obrigatório`,
 });
 
