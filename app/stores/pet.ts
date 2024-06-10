5966feb0e3bcac8b1a0375bccf0c99c1cfccc552
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-10 16:46:41 -0300

    Adiciona método para recuperar posts

--- app/stores/pet.ts
+++ app/stores/pet.ts
@@ -1,19 +1,21 @@
 import { defineStore } from "pinia";
+import { number } from "zod";
 import { useAuthStore } from "~/stores/auth";
-interface Message {
-  content: string;
-  messageOwner: boolean;
-}
 
 export const usePetStore = defineStore({
   id: "myPetStore",
   state: () => ({
-    posts: [] as Message[],
+    //posts: [] as Message[],
     loading: false,
     error: "",
+    cacheDuration: 300000,
+    cachedPosts: reactive({
+      data: [],
+      lastFetched: null,
+    }),
   }),
   actions: {
-    async registerPet(content) {
+    async registerPet(content: FormData) {
       try {
         this.error = "";
         this.loading = true;
@@ -43,6 +45,32 @@ export const usePetStore = defineStore({
       } catch (error) {
         console.log(error);
       }
+    },
+
+    async getPetPosts(numberOfPosts = 3, page = 1) {
+      const now = new Date().getTime();
+      if (
+        !this.cachedPosts.lastFetched ||
+        now - this.cachedPosts.lastFetched > this.cacheDuration ||
+        this.cachedPosts.data.length !== numberOfPosts
+      ) {
+        const token = useCookie("token");
+        const response = await fetch(
+          `http://localhost:7070/api/pet/posts?paginate=${numberOfPosts}&page=${page}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${token.value}`,
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        this.cachedPosts.data = data;
+        this.cachedPosts.lastFetched = now;
+      }
+      return this.cachedPosts;
     },
   },
 });
