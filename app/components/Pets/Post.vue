1b30035acc3f2a6f7682320f1a2e4ad84004b198
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-10 18:32:22 -0300

    Adiciona componente de posts de pets

---
+++ app/components/Pets/Post.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex flex-col w-full items-center gap-3 pb-4">
+      <span>Nome do pet desparecido: {{ petPost.name }}</span>
+      <div class="w-full p-2 flex items-center justify-center">
+        <img :src="url" class="md:w-[50%] h-[100px] md:h-[200px]" />
+      </div>
+      <Button @click="toggleChat()">Contatar o dono</Button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["toggleChat"]);
+
+const props = defineProps({
+  petPost: {
+    type: Object,
+    required: true,
+  },
+});
+
+function toggleChat() {
+  emit("toggleChat", props.petPost.user_id);
+}
+const url =
+  "http://localhost:7070/" + props.petPost.pet_post_images[0].image_path;
+</script>
+
+<style></style>
