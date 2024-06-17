cf2090ce9d5049dae03d42599506c549e56f8ccc
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2024-06-17 15:44:04 -0300

    Adiciona envio do id do usuario a mensagem

--- app/plugins/websocket.ts
+++ app/plugins/websocket.ts
@@ -20,7 +20,12 @@ export default defineNuxtPlugin((nuxtApp) => {
       console.log(event.data);
       if (data.type === "new_user" || data.type === "new_message") {
         const store = useChatStore();
-        store.setMessage(data.message, +data.user_id === +userId, data.to);
+        store.setMessage(
+          data.message,
+          data.user_id,
+          +data.user_id === +userId,
+          data.to
+        );
       }
     };
 
